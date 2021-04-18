2d4faf5dbd1092a7360e739a2d29b17302796529
pabnaar1 <78859556+pabnaar1@users.noreply.github.com>
2021-04-18 15:42:56 +0200

    Project 3
    Project3

---
+++ Assets/script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider))]
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    public string parametroBoolean;
+    bool b = false;
+    private void OnTriggerEnter(Collider Other)
+    {
+        b = !b;
+        animator.SetBool(parametroBoolean, b);
+    }
+}
