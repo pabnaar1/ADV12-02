75559ea8b93423cad52369c2131956d6d261b6f8
pabnaar1 <78859556+pabnaar1@users.noreply.github.com>
2021-04-25 21:04:37 +0200

    Lesson9
    Mutant character

---
+++ Assets/MutantScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MutantScript : MonoBehaviour
+{
+    public float Downancell = 1f;  
+    private Animator animator;
+    private CharacterController charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        charController.Move(Vector3.down * Downancell * Time.deltaTime);
+        animator.SetFloat("Vertical", 0);
+        animator.SetFloat("Horizontal", 0);
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            animator.SetFloat("Vertical", 1);
+        }
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            animator.SetFloat("Vertical", -1);
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            animator.SetFloat("Horizontal", -1);
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            animator.SetFloat("Horizontal", 1);
+        }
+
+    }
+}
